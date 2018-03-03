89145a81c011cad8469330e0dd607f0a02e3e06c
Gerald Derrick <jerry.derrick@gmail.com>
2018-03-03 13:13:21 -0600

    New routes testing

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/books', function () {
+    return 'Here are all the books in the library';
+});
+
+Route::get('/books/{title}', function ($title) {
+    return 'You are looking at the book ' . $title;
+});
