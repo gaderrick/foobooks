b8bf65b0403d45943932c619077d5089d101d05f
Gerald Derrick <jerry.derrick@gmail.com>
2018-03-03 14:06:53 -0600

    More controller updates

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.7 on 2018-03-03 18:58:07.
+ * Generated for Laravel 5.6.7 on 2018-03-03 19:53:16.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'PageController@welcome');
+Route::get('/about', 'PageController@about');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/contact', 'PageController@contact');
 
-Route::get('/books', function () {
-    return 'Here are all the books in the library';
-});
-
-Route::get('/books/{title}', function ($title) {
-    return 'You are looking at the book ' . $title;
-});
+Route::get('/books','BookController@index');
+Route::get('/books/{title}', 'BookController@show');
